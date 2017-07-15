d9bf676edf20a6d823d02af109fa5710df5e7c4b
Einar Strandberg <einargs@gmail.com>
2017-07-15 17:38:37 +0200

    Added authentication guard and sign in prompt.
    The user is now prevented from accessing the recipe paths if they aren't
    signed in, and prompted to sign in.

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,18 +1,33 @@
-import { NgModule }              from "@angular/core";
-import { RouterModule, Routes }  from "@angular/router";
+import { NgModule }               from "@angular/core";
+import { RouterModule, Routes }   from "@angular/router";
 
-import { RecipeListComponent }   from "./recipe-list/recipe-list.component";
-import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeListComponent }    from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent }  from "./recipe-detail/recipe-detail.component";
+
+import { UserService }            from "../user/user.service";
+import { RequireUserGuard }       from "../user/require-user-guard.service";
 
 const recipesRoutes: Routes = [
-  { path: "recipe", component: RecipeListComponent },
-  { path: "recipe/:id", component: RecipeDetailComponent }
+  {
+    path: "recipe",
+    component: RecipeListComponent,
+    canActivate: [RequireUserGuard]
+  },{ 
+    path: "recipe/:id",
+    component: RecipeDetailComponent,
+    canActivate: [RequireUserGuard]
+  }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forChild(recipesRoutes)
   ],
-  exports: [ RouterModule ]
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    RequireUserGuard
+  ]
 })
 export class RecipesRoutingModule {}
---
+++ src/app/user/require-user-guard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable }   from "@angular/core";
+import {
+  CanActivate, Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+}                       from "@angular/router";
+
+import { UserService }  from "./user.service";
+
+@Injectable()
+export class RequireUserGuard implements CanActivate {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  // Prompt the user to sign in; on success redirect them to the requested page
+  private async promptSignInAndRedirect(redirectUrl: string) {
+    await this.userService.signInPopup();
+    await this.router.navigateByUrl(redirectUrl);
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.userService.signedIn) {
+      return true;
+    } else {
+      this.promptSignInAndRedirect(state.url);
+      return false;
+    }
+  }
+}
